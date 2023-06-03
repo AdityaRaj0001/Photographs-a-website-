10962d740c9a7f9b12102a62a66fb8bee04c6cbf
Aditya raj <92699283+AdityaRaj0001@users.noreply.github.com>
2023-06-03 18:22:31 +0530

    First Commit
    uploading all files for the first time

---
+++ userinfo.php
@@ -0,0 +1,20 @@
+<?php
+echo "hello";
+/*
+$con=mysqli_connect('localhost','root');
+
+mysquli_select_db($connection,'clickshots data');
+
+$USER=$_POST['USER'];
+
+$E-mail=$_POST['E-mail'];
+
+$Mobile=$_POST['Mobile'];
+
+$Comment=$_POST['Comment'];
+
+$query = "INSERT INTO 'feedback' ("USER","E-mail","Mobile","Comment") VALUES ('$USER','$E-mail','$Mobile','$Comment') ";
+
+mysqli_query($con,$query);
+*/
+?>
